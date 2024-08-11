e1523016a976959313772b34cf3e5f36e8326f2d
Diallo <mamadouadama609@gmail.com>
2024-08-10 20:30:43 -0500

    db postgres with docker and user app

--- employeeproject/employeeproject/settings.py
+++ employeeproject/employeeproject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'api'
+    'api',
+    'user'
     
 ]
 
@@ -79,10 +80,15 @@ WSGI_APPLICATION = 'employeeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'example',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
